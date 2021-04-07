769be49018e099c7dda2cc099573359a86ca5bdf
Pontalier Marie <mpontali@insa-20729>
2021-04-07 15:16:51 +0200

    fin seance

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,6 +1,21 @@
 package org.insa.graphs.algorithm.shortestpath;
+import java.util.ArrayList;
+import org.insa.graphs.model.*;
+import org.insa.graphs.algorithm.utils.*;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
+	
+	private class Label implements Comparable<Label> {
+		private Node sommet_courant;
+		private boolean marque;
+		private double cout;
+		private Arc arc_precedent;
+		public double getCost() {return cout;};
+		@Override
+		public int compareTo(Label autre) {
+			return (Double.compare(this.cout, autre.cout));
+		}
+	}
 
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
@@ -10,7 +25,16 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        Graph graph = data.getGraph();
+        Label tab_label[]= new Label[graph.size()];
+        for (int i=0; i<graph.size();i++) {
+        	tab_label[i].sommet_courant=graph.get(i);
+        	tab_label[i].marque=false;
+        	tab_label[i].cout=Double.MAX_VALUE;
+        }
+        tab_label[data.getOrigin().getId()].cout = 0;
+        BinaryHeap<Label> tas= new BinaryHeap<Label>(); 
+        
         return solution;
     }
 
